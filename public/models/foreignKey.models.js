518869beaf7d52f7e100b9153a795b1cafe41a19
daviandany <andanydavi2@gmail.com>
2026-02-12 14:36:48 -0300

    feat: models/foreignKey

---
+++ public/models/foreignKey.models.js
@@ -0,0 +1,29 @@
+import sequelize from '../config/database.js';
+import User from './UserModels.js';
+import Task from './TaskModels.js';
+
+User.hasMany(Task, {
+    foreignkey: 'userId',
+    as: 'tasks',
+    onDelete: 'CASCADE',
+});
+
+Task.belongsTo(User, {
+    foreignkey: 'key',
+    as: 'user',
+});
+
+const syncDatabase = async () => {
+    try{
+        await sequelize.authenticate();
+        console.log('Conexão com o banco de dados estabelecida')
+
+        await sequelize.sync({ alter: true });
+        console.log('Tabelas sincronizadas')    
+    } catch (error) {
+        console.log('Erro ao conectar no banco: ', error.message);
+        process.exit(1)
+    }
+};
+
+module.exports = { sequelize, User, Task, syncDatabase }
