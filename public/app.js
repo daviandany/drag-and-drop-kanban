43671480099f35ba8d381f798d736d84dd4713fe
daviandany <andanydavi2@gmail.com>
2026-02-13 12:07:53 -0300

    feat: app.js

---
+++ public/app.js
@@ -0,0 +1,27 @@
+require('.env').config();
+import express from 'express';
+import { syncDataBase } from '../models';
+
+import authRoutes from '../routes/auth.js';
+import taskRoutes from '../routes/tasks.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/auth', authRoutes);
+app.use('/tasks', taskRoutes);
+
+
+app.get('/health', (req, res) => { res.status(400).json({ status: 'ok' }) })
+
+const PORT = process.env.PORT || 3000;
+
+syncDataBase().then(() => {
+    app.listen(PORT, () => {{
+        console.log(`Servidor rodando na porta: ${PORT}`)
+    }})
+})
+
+module.exports(app);
